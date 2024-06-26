2920d1c1afa06d7e8c5fc57864e9fcdd9e132d73
Udit Gurani <gurani.udit3@gmail.com>
2024-06-26 21:18:42 +0530

    day-13

---
+++ beginner/day-13.py
@@ -0,0 +1,26 @@
+# Describe Problem
+# def my_function():
+#     for i in range(1, 21):
+#         if i == 20:
+#             print("you got it")
+#
+#
+# my_function()
+
+
+# from random import randint
+# dice_imgs = ["1","2","3","4","5","6","7","8","9"]
+# dice_num = randint(1,6)
+# print(dice_imgs[dice_num])
+
+# year = int(input("What's your year of birth?"))
+# if 1980 < year < 1994:
+#     print("You are a millenial.")
+# elif year >= 1994:
+#     print("You are a Gen Z.")
+
+# age = int(input("How old are you?"))
+# if age > 18:
+#     print(f"You can drive at age {age}.")
+
+
